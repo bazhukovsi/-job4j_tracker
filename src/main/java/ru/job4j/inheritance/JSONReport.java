b313a1eba8351dd3ab65a0309b9195541c64e133
Сергей <bazhukovsi@yandex.ru>
2021-11-30 18:09:18 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,18 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" : \"" + name + "\"," + ln
+                + "\t\"body\" : \"" + body + "\"" + ln
+                + "}";
+    }
+
+    public static void main(String[] args) {
+        TextReport textReport = new JSONReport();
+        System.out.println(textReport.generate("Serg Bazhukov",
+                "i like to play table tennis"));
+    }
+}
