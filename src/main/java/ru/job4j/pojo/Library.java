7d8d6d30a09af424489f8892b1723be3da79ee81
Сергей <bazhukovsi@yandex.ru>
2021-12-01 12:40:40 +0300

    3. Массивы и модели. [#174892 #234491]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -23,7 +23,7 @@ public class Library {
         }
         System.out.println("=============================");
         for (int i = 0; i < books.length; i++) {
-            if (books[i].equals("Clean code")) {
+            if (books[i].getName().equals("Clean code")) {
                 System.out.println(books[i].getName() + " - " + books[i].getCount());
             }
         }
