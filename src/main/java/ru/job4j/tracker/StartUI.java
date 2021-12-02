f00a45a9f9f581dee4f4f17edd4cdc85177d758e
Сергей <bazhukovsi@yandex.ru>
2021-12-02 07:37:11 +0300

    9. toString [#310018 #234859]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println(item.toString());
+        System.out.println(item);
 
     }
 }
