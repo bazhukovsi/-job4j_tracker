dc064c41ee71dbfda760417029e9e3622a36d373
Сергей <bazhukovsi@yandex.ru>
2021-12-25 21:43:04 +0300

    4. Преобразование List в Map. [#110226]

---
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+    public static Map<String, Student> convert(List<Student> list) {
+        return list.stream().collect(Collectors.toMap(Student::getSurname, s -> s, (s1, s2) -> s1));
+    }
+}
